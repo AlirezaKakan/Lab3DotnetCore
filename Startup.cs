7a74f85c57dfbb9fa245d59a20d270dbbfa85399
alireza <alireza.kakan@gmail.com>
2021-01-26 16:45:37 -0800

    add the migration method

--- Startup.cs
+++ Startup.cs
@@ -38,7 +38,7 @@ namespace Lab03
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -66,6 +66,8 @@ namespace Lab03
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            context.Database.Migrate();
         }
     }
 }
